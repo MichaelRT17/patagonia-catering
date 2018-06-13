7c3880f8dd1e0e7026060c54a379e0ba5a326f89
Michael Thurman <michaelrt17@icloud.com>
2018-06-13 13:25:53 -0600

    created navbar functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './letter-p.png';
+import Icon from '@material-ui/core/Icon';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
-export default function Header() {
-    return (
-        <div className='header-block'>
-            <img src='./letter-p.png' alt=''/>
-        </div> 
-    )
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            navmenu: false
+        }
+    }
+
+    handleNavClick() {
+        this.setState({
+            navmenu: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <header className='header-block'>
+                    <Link to='/' >
+                        <div className='icon-box'>
+                            <img height='50px' src={logo} alt='' />
+                            <h3>atigonia</h3>
+                        </div>
+                    </Link >
+                    <Icon style={{ fontSize: 40, color: '#F6B506' }}
+                        onClick={() => this.setState({ navmenu: !this.state.navmenu })}>
+                        add_circle
+                    </Icon >
+                </header>
+                <nav className={this.state.navmenu ? "navbar" : "hiddennavbar"}>
+                    <Link to='/about' onClick={() => this.handleNavClick()}>
+                        <h4 className={this.state.navmenu ? "dropmenu" : "dropmenuhidden"}>About</h4>
+                    </Link >
+                    <Link to='/contact' onClick={() => this.handleNavClick()}>
+                        <h4 className={this.state.navmenu ? "dropmenu" : "dropmenuhidden"}>Contact</h4>
+                    </Link>
+                    <Link to='/cart' onClick={() => this.handleNavClick()}>
+                        <h4 className={this.state.navmenu ? "dropmenu" : "dropmenuhidden"}>Cart</h4>
+                    </Link>
+                </nav>
+            </div>
+        )
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
 import Cart from './components/Cart/Cart';
+import About from './components/About/About';
 
 export default (
     <Switch >
         <Route exact path='/' component={Home} />
         <Route path='/contact' component={Contact} />
         <Route path='/cart' component={Cart} />
+        <Route path='/about' component={About} />
     </Switch >
 )
