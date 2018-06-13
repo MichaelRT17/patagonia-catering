beaa63f790ae7a3cb475ba3dbbaa2453e366ebcc
Michael Thurman <michaelrt17@icloud.com>
2018-06-12 22:02:53 -0600

    second push

---
+++ db/db_seeds/create_product_seed_example.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (product_name, product_img, product_price, quantity, feeds, category, product_desc)
+VALUES ('Chicken Milanesas', 'https://assets.epicurious.com/photos/57b208aaa68a724e381a98a7/2:1/w_1260%2Ch_630/lemony-chicken-milanese-with-arugula-salad.jpg', 80.00,
+ 50, '30-40', 'Main Dish', 'Thinly-sliced, breaded chicken, marinated with herbs, spices and lemon.')
